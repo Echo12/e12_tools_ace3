c7d48a36a513fd5361f3e958f7e496eafb6045ee
Trigger81 <Trigger81@users.noreply.github.com>
2015-06-04 22:04:11 +0200

    fixes

--- mod.cpp
+++ mod.cpp
@@ -1,12 +1,12 @@
 name = "Echo12 Tools version 3.0";
-picture = "logo_echo12.paa";
+picture = "logo_echo12_ca.paa";
 actionName = "Echo12";
 action = "https://www.echo12.de";
 description = "Documentation: http://wiki.echo12.de/wiki/E12_Tools";
-logo = "logo_echo12.paa";
-logoOver = "logo_echo12.paa";
+logo = "logo_echo12_ca.paa";
+logoOver = "logo_echo12_ca.paa";
 tooltip = "E12";
 tooltipOwned = "E12 Owned";
 overview = "E12 Tools for Arma 3";
 author = "Echo12 Development Team";
-overviewPicture = "logo_echo12.paa";
+overviewPicture = "logo_echo12_ca.paa";
